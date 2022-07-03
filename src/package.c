0c547c00e996d29e42c3a20f5502121c0d12ff96
Michael Gene Brockus <55331536+troglobyte-coder@users.noreply.github.com>
2022-07-03 14:07:31 -0700

    Initial commit

---
+++ src/package.c
@@ -0,0 +1,19 @@
+/*
+   Troglobyte stdlib:
+   author: Michael Gene Brockus
+   gmail: <michaelbrockus@gmail.com>
+*/
+#include "troglobyte/package.h"
+
+/*
+  overview:
+  > Should output the a greeting message
+  > to the user.
+  >
+  args:
+  -> ...: N/A
+*/
+const char *greet(void)
+{
+    return "Hello, C Developer.";
+} // end of func
